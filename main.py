80a22ad33885e908f2e13296b9fb236604b618d0
Arthur Jacquin <arthur.jacquin@student-cs.fr>
2023-01-25 08:56:49 +0000

    add: trace validation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from math import inf
+from random import randint
 
 class Automaton:
 
@@ -10,9 +11,8 @@ class Automaton:
         ''' Create an automaton from the data contained in input_file '''
         with open(input_file, 'r') as file:
             self.S = file.readline().strip().split(' ')
-            self.s0, self.t = file.readline().strip().split(' ')
+            self.s0 = file.readline().strip()
             self.check(self.s0)
-            self.check(self.t)
             self.T = {}
             self.L = []
             for tr in file:
@@ -21,9 +21,7 @@ class Automaton:
                 self.check(dest)
                 self.T[(src, label)] = dest
                 self.L.append(label[1:])
-        self.state = self.s0
         self.is_bipartite = False
-        self.ranks_computed = False
 
     def to_bipartite(self):
         ''' Transform the automaton into a bipartite one '''
@@ -73,14 +71,7 @@ class Automaton:
         # Remember the process has been done
         self.is_bipartite = True
 
-    def move(self, label):
-        ''' Move to the next state '''
-
-        if (self.state, label) not in self.T:
-            raise f'Unable to process "{label}" from node {self.state}.'
-        self.state = self.T[(self.state, label)]
-
-    def compute_ranks(self):
+    def compute_ranks(self, obj):
         ''' Compute the ranks '''
 
         # Get sure the graph is bipartite
@@ -88,56 +79,53 @@ class Automaton:
             self.to_bipartite()
 
         # Init ranks
-        self.ranks = {}
-        self.ranks[self.t] = 0
+        ranks = {}
+        ranks[obj] = 0
         k = 1
 
         added = True
         while added:
             added = False
             for p in self.V:
-                if p in self.ranks:
+                if p in ranks:
                     continue
                 if self.marks[p] == -1:
                     for q in self.E[p]:
-                        if q in self.ranks:
-                            self.ranks[p] = k
+                        if q in ranks:
+                            ranks[p] = k
                             added = True
                             break
                 else:
                     for q in self.E[p]:
-                        if q not in self.ranks:
+                        if q not in ranks:
                             break
                     else:
-                        self.ranks[p] = k
+                        ranks[p] = k
                         added = True
             if added == False:
                 # W(i+1, j) = W(i, j): we need to increment j
                 for p in self.V:
-                    if p not in self.ranks and self.marks[p] == 1:
+                    if p not in ranks and self.marks[p] == 1:
                         is_found_in = False
                         is_found_not_in = False
                         for q in self.E[p]:
-                            if q in self.ranks:
+                            if q in ranks:
                                 is_found_in = True
                             else:
                                 is_found_not_in = True
                         if is_found_in == True and is_found_not_in == True:
-                            self.ranks[p] = k
+                            ranks[p] = k
                             added = True
             k += 1
 
-        # Remember ranks has been computed
-        self.ranks_computed = True
+        # Return the results
+        return ranks
 
-    def winning_strategy(self):
+    def winning_strategy(self, obj):
         ''' Compute the winning strategy '''
 
-        # Get sure ranks has been computed
-        if (self.ranks_computed == False):
-            self.compute_ranks()
-
         res = {}
+        ranks = self.compute_ranks(obj)
 
         for node in self.V:
             if self.marks[node] == -1:
@@ -150,90 +138,193 @@ class Automaton:
                     for (src, label), dest in self.T.items():
                         if src != node:
                             continue
-                        if lowest_rank == None or (self.ranks[dest] < lowest_rank):
+                        if lowest_rank == None or (ranks[dest] < lowest_rank):
                             tr = label
-                            lowest_rank = self.ranks[dest]
+                            lowest_rank = ranks[dest]
                     res[node] = tr
 
         return res
+    
+    def blue_move(self, state, strategy):
+        return self.T[(state, strategy[state])]
 
-    def compute_couples_ranks(self):
-        ''' Compute couples ranks '''
+    def red_move(self, state):
+        return self.E[state][randint(0, len(self.E[state])-1)]
 
-        # Get sure the graph is bipartite
-        if not(self.is_bipartite):
-            self.to_bipartite()
+    def reproduce(self, trace):
+        # Get the sequence of transitions to reproduce
+        transitions = []
+        with open(trace, 'r') as file:
+            for label in file:
+                transitions.append(file.readline().strip())
 
-        #Initialize W and the rank dictionary
-        self.W = [[],[self.t]]
-        Wmemory = [] #List keeping in memory the previous iteration of W[-1]
-        self.rankdic = {self.t: (0,0)}
-        i,j = 0,0
+        # Get the state before first transition
+        for (src, label) in self.T:
+            if label == transitions[0]:
+                init_state = src
+                break
 
-        while self.W[-1] != Wmemory:
-            j+=1
-            Wmemory = self.W[-1][:]
-            for p in filter(lambda x: x not in Wmemory, self.V):
-                if self.marks[p]==-1:
-                    for q in self.E[p]:
-                        if q in Wmemory:
-                            self.W[-1].append(p)
-                            self.rankdic[p] = (i,j)
-                            break
+        # Compute strategies
+        strategy_s0 = self.winning_strategy(self.s0)
+        strategy_init = self.winning_strategy(init_state)
+        
+        # Try to reproduce trace
+        while True:
+            # Init
+            print("Trying to read the trace")
+            state = self.s0
+            print(state)
+
+            # Reach init_state
+            while True:
+                # Blue turn
+                if state == "errorB":
+                    break
                 else:
-                    b = True
-                    for q in self.E[p]:
-                        if self.marks[q]==-1 and q not in Wmemory:
-                            b = False
-                            break
-                    if b:
-                        self.W[-1].append(p)
-                        self.rankdic[p] = (i,j)
+                    state = self.blue_move(state, strategy_init)
+                    print(state)
+                    if state == init_state:
+                        player = 1
+                        break
+                # Red turn
+                state = self.red_move(state)
+                print(state)
+                if state == init_state:
+                    player = -1
+                    break
 
-        while self.W[-1] != self.W[-2]:
-            i+=1
-            j=0
-            self.W.append(self.W[-1][:])
-            for p in filter(lambda x: x not in self.W[-2], self.V):
-                if self.marks[p]==1:
-                    b1,b2 = False,False
-                    for q in self.E[p]:
-                        if q in self.W[-2]:
-                            b1 = True
-                        else:
-                            b2 = True
-                    if b1 and b2:
-                        self.W[-1].append(p)
-                        self.rankdic[p] = (i,j)
+            if state == "errorB":
+                print("Unexpected comportement of red. Abortion of try.")
+                continue
 
-            while self.W[-1] != Wmemory:
-                j+=1
-                Wmemory = self.W[-1][:]
-                for p in filter(lambda x: x not in Wmemory, self.V):
-                    if self.marks[p]==-1:
-                        for q in self.E[p]:
-                            if q in Wmemory:
-                                self.W[-1].append(p)
-                                self.rankdic[p] = (i,j)
-                                break
+            # Try to follow the transitions
+            deviated = False
+            for label in transitions:
+                if player == -1:
+                    if (state, label) not in self.T:
+                        deviated = True
+                        break
                     else:
-                        b = True
-                        for q in self.E[p]:
-                            if self.marks[q]==-1 and q not in Wmemory:
-                                b = False
-                                break
-                        if b:
-                            self.W[-1].append(p)
-                            self.rankdic[p] = (i,j)
-        self.W.pop(0)
-        self.W.pop()
-        for p in filter(lambda x: x not in self.W[-1], self.V):
-            self.rankdic[p]= (inf,inf)
+                        state = self.T[(state, label)]
+                        print(state)
+                else:
+                    state = self.red_move(state)
+                    print(state)
+                player = -player
+
+            if deviated:
+                print("Deviation from trace. Abortion of try.")
+                continue
+            else:
+                player = -player
+
+            # Reach s0
+            while True:
+                if player == -1:
+                    # Blue turn
+                    if state == "errorB":
+                        break
+                    else:
+                        state = self.blue_move(state, strategy_s0)
+                        print(state)
+                else:
+                    # Red turn
+                    state = self.red_move(state)
+                    print(state)
+                if state == self.s0:
+                    break
+                player = -player
+            
+            if state == "errorB":
+                print("Unexpected comportement of red. Abortion of try.")
+                continue
+            
+            # Win
+            print("The trace has been read and is ready to be read again!")
+            return
+
+
+    # def compute_couples_ranks(self):
+    #     ''' Compute couples ranks '''
+
+    #     # Get sure the graph is bipartite
+    #     if not(self.is_bipartite):
+    #         self.to_bipartite()
+
+    #     #Initialize W and the rank dictionary
+    #     self.W = [[],[self.t]]
+    #     Wmemory = [] #List keeping in memory the previous iteration of W[-1]
+    #     self.rankdic = {self.t: (0,0)}
+    #     i,j = 0,0
+
+    #     while self.W[-1] != Wmemory:
+    #         j+=1
+    #         Wmemory = self.W[-1][:]
+    #         for p in filter(lambda x: x not in Wmemory, self.V):
+    #             if self.marks[p]==-1:
+    #                 for q in self.E[p]:
+    #                     if q in Wmemory:
+    #                         self.W[-1].append(p)
+    #                         self.rankdic[p] = (i,j)
+    #                         break
+    #             else:
+    #                 b = True
+    #                 for q in self.E[p]:
+    #                     if self.marks[q]==-1 and q not in Wmemory:
+    #                         b = False
+    #                         break
+    #                 if b:
+    #                     self.W[-1].append(p)
+    #                     self.rankdic[p] = (i,j)
+
+    #     while self.W[-1] != self.W[-2]:
+    #         i+=1
+    #         j=0
+    #         self.W.append(self.W[-1][:])
+    #         for p in filter(lambda x: x not in self.W[-2], self.V):
+    #             if self.marks[p]==1:
+    #                 b1,b2 = False,False
+    #                 for q in self.E[p]:
+    #                     if q in self.W[-2]:
+    #                         b1 = True
+    #                     else:
+    #                         b2 = True
+    #                 if b1 and b2:
+    #                     self.W[-1].append(p)
+    #                     self.rankdic[p] = (i,j)
+
+    #         while self.W[-1] != Wmemory:
+    #             j+=1
+    #             Wmemory = self.W[-1][:]
+    #             for p in filter(lambda x: x not in Wmemory, self.V):
+    #                 if self.marks[p]==-1:
+    #                     for q in self.E[p]:
+    #                         if q in Wmemory:
+    #                             self.W[-1].append(p)
+    #                             self.rankdic[p] = (i,j)
+    #                             break
+    #                 else:
+    #                     b = True
+    #                     for q in self.E[p]:
+    #                         if self.marks[q]==-1 and q not in Wmemory:
+    #                             b = False
+    #                             break
+    #                     if b:
+    #                         self.W[-1].append(p)
+    #                         self.rankdic[p] = (i,j)
+    #     self.W.pop(0)
+    #     self.W.pop()
+    #     for p in filter(lambda x: x not in self.W[-1], self.V):
+    #         self.rankdic[p]= (inf,inf)
 
 
 A = Automaton('input_file')
 
-print(A.winning_strategy())
+# print(A.winning_strategy("s3"))
+
+# print(A.E)
+
+A.reproduce('trace')
 
 # A.compute_couples_ranks()
 # print(A.rankdic)
